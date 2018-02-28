dc4aee7b280d47600e698d9887a3da7ff666d4eb
sunkstingray <lynam.chris@gmail.com>
2018-02-28 13:02:34 -0600

    fixed a few typos

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
-const bookRoutes = require("./books");
+const bookRoutes = require("./content");
 
 // Book routes
-router.use("/books", bookRoutes);
+router.use("/conent", bookRoutes);
 
 module.exports = router;
