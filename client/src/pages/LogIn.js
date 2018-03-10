f4e02024782ae70954bcb463d09473f5b76bbf07
jnguye89 <Jackie@Jackies-MacBook-Pro.local>
2018-03-10 11:06:22 -0600

    Card and file changes

--- client/src/pages/LogIn.js
+++ client/src/pages/LogIn.js
@@ -61,40 +61,42 @@ class LogIn extends Component {
 		} else {
       return(
         <div className="container">
+          <Card>
             <h1>Log in to your account.</h1>
-          <form>
-            <div className="form-group">
-              <label htmlFor="emailInput">Email address</label>
-              <input
-                type="email"
-                className="form-control"
-                id="emailInput"
-                aria-describedby="emailHelp"
-                name="username"
-							  value={this.state.username}
-							  onChange={this.handleChange}
-              />
-              <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-            </div>
-            <div className="form-group">
-              <label htmlFor="exampleInputPassword1">Password</label>
-              <input
-                type="password"
-                className="form-control"
-                id="exampleInputPassword1"
-                name="password"
-							  value={this.state.password}
-							  onChange={this.handleChange}
-              />
-            </div>
-            <Buttons onClick={this.handleSubmit}>Login</Buttons>
-          </form>
+            <form>
+              <div className="form-group">
+                <label htmlFor="emailInput">Email address</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="emailInput"
+                  aria-describedby="emailHelp"
+                  name="username"
+                  value={this.state.username}
+                  onChange={this.handleChange}
+                />
+                <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+              </div>
+              <div className="form-group">
+                <label htmlFor="exampleInputPassword1">Password</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="exampleInputPassword1"
+                  name="password"
+                  value={this.state.password}
+                  onChange={this.handleChange}
+                />
+              </div>
+              <Buttons onClick={this.handleSubmit}>Login</Buttons>
+            </form>
           {/* <Card>
           <a href="/auth/google">
 						<GoogleButton />
 						<img src={googleButton} alt="sign into Google Button" />
 					</a>
           </Card> */}
+          </Card>
         </div>
       );
     }
